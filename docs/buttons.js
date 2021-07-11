54077f6c631eff1d69eb50558c3b3b7efcc7dd58
grem1ly <emmstrand@ucdavis.edu>
2021-07-11 02:55:44 -0700

    update template

--- docs/buttons.js
+++ docs/buttons.js
@@ -1,4 +1,9 @@
 
+document.getElementById("reshuffle1").addEventListener("click", function() {
+  reshuffle("1")});
+document.getElementById("reshuffle2").addEventListener("click", function() {
+  reshuffle("2")});
+
 document.getElementById("add_links?").addEventListener("click", generate_links);
 document.getElementById("final_connection").addEventListener("click", link_the_actors);
 
@@ -6,5 +11,9 @@ document.getElementById("head_link_button1").addEventListener("click", function(
   open_hidden_panel(this.id)});
 document.getElementById("head_link_button2").addEventListener("click", function() {
   open_hidden_panel(this.id)});
+document.getElementById("back_button").addEventListener("click", return_home);
 
 document.getElementById("submit_link").addEventListener("click", add_link);
+
+document.getElementById("overlay").addEventListener("click", return_home);
+document.getElementById("close_out").addEventListener("click", return_home);
